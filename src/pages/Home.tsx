282c3190ca1a1e6d9f2149fee16c895d77afa97e
Pranto Deb <rajprit0m@outlook.com>
2025-04-29 00:09:59 +0600

    [Fonts, Tailwind CSS, DaisyUI]:[Install]

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,12 @@
 const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Home</h1>
+      <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">
+        Responsive
+      </button>
+    </div>
+  );
 };
 
 export default Home;
