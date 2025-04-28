765ab994873b2dbb30cb4415622f29497c8dc1b0
Pranto Deb <rajprit0m@outlook.com>
2025-04-28 23:58:10 +0600

    [Project Setup][10]

--- src/routers/Routes.tsx
+++ src/routers/Routes.tsx
@@ -1,9 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../layouts/Root";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 export const routes = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
   },
 ]);
